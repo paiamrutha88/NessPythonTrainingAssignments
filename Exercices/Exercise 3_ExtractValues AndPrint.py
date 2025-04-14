3f7b291bda8511d72fd431c724dbf3674e1e38c2
paiamrutha88 <paiamrutha88@gmail.com>
2025-04-14 12:35:39 +0530

    Exercise 3 completed

--- Exercices/Exercise 3_ExtractValues AndPrint.py
+++ Exercices/Exercise 3_ExtractValues AndPrint.py
@@ -1,2 +1,14 @@
-sentence = "123.123.123.123 - - [26/Apr/2000:00:23:48 -0400] GET /pics/wpaper.gif HTTP/1.0"200 6248
- http://www.jafsoft.com/asctortf/ Mozilla/4.05 (Macintosh; I; PPC)"
+import re
+
+sentence = """123.123.123.123 - - [26/Apr/2000:00:23:48 -0400] "GET /pics/wpaper.gif HTTP/1.0"200 6248
+ "http://www.jafsoft.com/asctortf/" "Mozilla/4.05 (Macintosh; I; PPC)"""
+
+ip = re.search(r'^\d{1,3}(?:\.\d{1,3}){3}',sentence).group()
+date = re.search(r'\[(.*?)\]',sentence).group(1)
+image= re.search(r'GET \/pics\/\S+',sentence).group()
+url = re.search(r'"(http[s]?://[^"]+)"',sentence).group(1)
+
+print(ip)
+print(date)
+print(image)
+print(url)
